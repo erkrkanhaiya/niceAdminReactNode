b569d27d298b7d0003305ba89cabead8e72c26a5
erkrkanhaiya <erkrkanhaiya@gmail.com>
2021-08-16 01:19:25 +0530

    add vercel file4

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(morgan('tiny'));
 // Connect to DB
 connectDB();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 // Middleware
 // app.use(express.json())
